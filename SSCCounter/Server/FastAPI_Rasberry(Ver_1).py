f9a3fd23f9449496439952a788f71ef2e42a7ce1
AI-WonYJ <yjason7587@naver.com>
2022-11-25 03:30:09 +0900

    Rasberry Pi 3 Post

--- SSCCounter/Server/FastAPI_Rasberry(Ver_1).py
+++ SSCCounter/Server/FastAPI_Rasberry(Ver_1).py
@@ -1,9 +1,7 @@
 # ============ Set ============
 
 #  -*- coding: utf-8 -*-
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from datetime import datetime
 
@@ -22,14 +20,11 @@ from datetime import datetime
 # 웹 출력
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/SSCC_INFO")
 async def POST_Sensor():
-    # check()
-    temp, hum, lamp, ncnt_people, standard_time = 0, 0, 0, 0, "Testing"
-    current_time = str(datetime.now() )[0:21]  # 필요한 부분 가공
-    print(ncnt_people)
-    return {"temperature": temp, "humidity": hum, "lamp": lamp,"people_count": ncnt_people, "last_time": current_time, "get_time": standard_time}
+    # Data_Get()  People_Number, Measure_Time, Temperature, Humidity, Lamp
+    People_Number, Measure_Time, Temperature, Humidity, Lamp = 100, "2022-11-25 03:01:10", 24, 80, "On"
+    Current_Time = str(datetime.now() )[0:21]  # 필요한 부분 가공
+    return {"Send_Time": Current_Time,"People_Number": People_Number, "Measure_Time": Measure_Time, "Temperature": Temperature, "Humidity": Humidity, "Lamp": Lamp}
