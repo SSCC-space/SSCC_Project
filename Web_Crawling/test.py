2294e80da4e8032d9113db90940d767347568d55
AI-WonYJ <yjason7587@naver.com>
2023-01-08 21:49:27 +0900

    컴퓨터 목록 엑셀파일

--- Web_Crawling/test.py
+++ Web_Crawling/test.py
@@ -1,9 +1,6 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-url = 'http://prod.danawa.com/list/?cate=112758&15main_11_02'
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-soup = BeautifulSoup(res.text, 'lxml') #가져온 문서를 lxml 객체로 만듦
-print(soup)
+import pandas as pd
+
+filename = "PRoduct.xlsx"
+df = pd.read_excel(filename, engine='openpyxl')
+
+print(df)
